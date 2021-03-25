0b5f798c3603e67c4e27d39e78d6d513f36e119c
FaniMdz <78092356+FaniMdz@users.noreply.github.com>
2021-03-25 13:23:55 -0600

    practicas_u2

---
+++ enviar.php
@@ -0,0 +1,16 @@
+<?php
+
+ 
+$nombre = $_GET["nombre"];
+$nacido = $_GET["nacido"];
+$hm = $_GET["hm"];
+$correo = $_GET["correo"];
+
+
+
+echo $nombre, "<br>";
+echo $nacido, "<br>";
+echo $hm, "<br>";
+echo $correo, "<br>";
+
+?>
